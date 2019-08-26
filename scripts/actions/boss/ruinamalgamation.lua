e931b1d940f137b37749b6f3ef4190aa6c7c1ccd
LunarEyeGaming <lunareyegaming@gmail.com>
2019-08-26 16:12:23 -0400

    Changes to the Ruin Seed, fixes and tweaks

--- scripts/actions/boss/ruinamalgamation.lua
+++ scripts/actions/boss/ruinamalgamation.lua
@@ -5,16 +5,13 @@
 
 function raBlinkEs(args, board)
   local variants = math.random(0, 4)
-  if variants == 1 and not animator.animationState("eye1") == "charged" then
+  if variants == 1 and animator.animationState("eye1") == "idle" then
 	animator.setAnimationState("eye1", "blink")
-  end
-  if variants == 2 and not animator.animationState("eye2") == "portalcharged" then
+  elseif variants == 2 and animator.animationState("eye2") == "idle" then
 	animator.setAnimationState("eye2", "blink")
-  end
-  if variants == 3 and not animator.animationState("eye3") == "charged" then
+  elseif variants == 3 and animator.animationState("eye3") == "idle" then
 	animator.setAnimationState("eye3", "blink")
-  end
-  if variants == 4 and not animator.animationState("eye4") == "charged" then
+  elseif variants == 4 and animator.animationState("eye4") == "idle" then
 	animator.setAnimationState("eye4", "blink")
   end
   
--- scripts/actions/everythingbutanimator.lua
+++ scripts/actions/everythingbutanimator.lua
@@ -3,27 +3,26 @@ require "/scripts/pathutil.lua"
 -- param variantNumber (number of variants)
 -- param type
 -- param state
--- The function of this is that it sets a somewhat random state type to an animation state and avoids state types that are in a specific state
+-- Sets the animation state of random 
 
 function raBlinkEs(args, board)
-  local highest = args.variantNumber
-  local variants = math.random(0, highest)
+  local variants = math.random(0, args.variantNumber)
   local stateType = args.stateType
   local selectedStateType = stateType..variants
   local state = args.toState
   
   if not variants == 0 then
     if not (variants == 1 and animator.animationState("eye1") == "charged") then
-	  animator.setAnimationState(stateType, state)
+	  animator.setAnimationState(args.stateType, state)
 	end
 	if not (variants == 2 and animator.animationState("eye2") == "portalcharged") then
-	  animator.setAnimationState(stateType, state)
+	  animator.setAnimationState(args.stateType, state)
 	end
 	if not (variants == 3 and animator.animationState("eye3") == "charged") then
-	  animator.setAnimationState(stateType, state)
+	  animator.setAnimationState(args.stateType, state)
 	end
 	if not (variants == 4 and animator.animationState("eye4") == "charged") then
-	  animator.setAnimationState(stateType, state)
+	  animator.setAnimationState(args.stateType, state)
 	end
   end
 end
