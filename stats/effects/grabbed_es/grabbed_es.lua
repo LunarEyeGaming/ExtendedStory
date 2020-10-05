732665c1e292775461e64e95518ae333ac0ffaa4
LunarEyeGaming <lunareyegaming@gmail.com>
2020-10-04 22:57:43 -0400

    Unstable version d4.10.20
    Dark Abyss Dimension Changes:
    * Added Ravenous Stalker
    * Connection to intervention established once again.

--- stats/effects/grabbed_es/grabbed_es.lua
+++ stats/effects/grabbed_es/grabbed_es.lua
@@ -1,11 +1,36 @@
 require "/scripts/status.lua"
+require "/scripts/vec2.lua"
 
 function init()
   local nearby = world.entityQuery(mcontroller.position(), 30, {includedTypes = {"monster", "npc"}, order = "nearest"})
+  grabOffset = config.getParameter("grabOffset", {0, 0})
+  grabDirection = config.getParameter("grabDirection", 1)
+  mcontroller.controlFace(grabDirection)
   target = nearby[1]
+  initialDuration = effect.duration()
+  
+  animator.setAnimationRate(0)
+  if status.isResource("stunned") then
+    status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
+  end
 end
 
 function update(dt)
+  currentDuration = effect.duration()
+  if currentDuration < initialDuration and currentDuration > 0 then
+    if not target or not world.entityExists(target) then
+      effect.expire()
+      return
+    end
+    effect.modifyDuration(initialDuration - currentDuration)
+  end
+
   local pos = world.entityPosition(target)
-  mcontroller.setPosition(pos)
+  mcontroller.setPosition(vec2.add(pos, grabOffset))
+  mcontroller.setVelocity({0, 0})
+  mcontroller.controlFace(grabDirection)
+  mcontroller.controlModifiers({
+      facingSuppressed = true,
+      movementSuppressed = true
+    })
 end
